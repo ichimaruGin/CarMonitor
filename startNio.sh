baa4a3301433c06924f1082fa7a89c07f31b174f
Jiezhen Yi <yijiezhen@gmail.com>
2013-08-07 10:19:39 +0400

    Update sh

--- startNio.sh
+++ startNio.sh
@@ -1,1 +1,1 @@
-java -cp "/home/jiezhen/.m2/repository/CarMonitor/CarMonitorCore/1.0/CarMonitorCore-1.0.jar:lib/*" org.zju.car_monitor.server.Server
+java -XX:-OmitStackTraceInFastThrow -cp "/home/jiezhen/.m2/repository/CarMonitor/CarMonitorCore/1.0/CarMonitorCore-1.0.jar:lib/*" org.zju.car_monitor.server.Server
